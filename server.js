a789783202f5980884984505923770b3db11e3d3
onggq37 <84619503+onggq37@users.noreply.github.com>
2022-02-10 16:38:23 +0800

    heroku

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
